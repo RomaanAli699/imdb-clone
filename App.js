1c2648eef7d895a7da1a7d39b85763ce3badde9f
RomaanAli699 <romaanali100@gmail.com>
2026-03-31 23:22:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJOSPcAK_jeEOmJoCZd30nz2KvzF_IkkOJ0Q&s" alt="" />
+
+      <div className="body">
+        <h3>Cars</h3>
+        <div className="data">
+          <p>Release Date: 2026</p>
+          <p>Rating: 4.5</p>
+        </div>
+
+      </div>
+    </div >
+  )
+}
+
+export default App
